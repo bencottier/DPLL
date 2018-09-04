63d8b6b5dd14d0759d6d743ea56133d98a5e96f1
Ben Cottier <bcottier2@gmail.com>
2018-09-05 00:03:15 +1000

    Revert "Work-in-progress for satisfiability conditional on number of true assignments"
    This reverts commit 57c3913

--- src/DPLL.java
+++ src/DPLL.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
 /**
  * Implementation of the Davis-Putnam-Logeman-Loveland (DPLL) algorithm.
  *
@@ -9,16 +5,13 @@ import java.util.List;
  */
 public class DPLL {
 
-    private static int numTrue = 3;
-
     /**
      * Determine whether the given sentence is satisfiable using DPLL.
      *
      * @param s sentence to analyse.
      * @return true if the sentence is satisfiable.
      */
-    public static boolean dpllSatisfiable(CNFSentence s,
-                                          List<Variable> assigned) {
+    public static boolean dpllSatisfiable(CNFSentence s) {
         System.out.println(s);
         if (s.isEmpty()) {
             return true;
@@ -28,37 +21,19 @@ public class DPLL {
         }
         Variable u = s.getUnitClause();
         if (u != null) {
-            System.out.println("Assign " + u.toString());
-            assigned.add(u);
-            return dpllSatisfiable(s.assign(u), assigned);
+            return dpllSatisfiable(s.assign(u));
         }
         u = s.getPureLiteral();
         if (u != null) {
-            System.out.println("Assign " + u.toString());
-            assigned.add(u);
-            return dpllSatisfiable(s.assign(u), assigned);
+            return dpllSatisfiable(s.assign(u));
         }
         Variable v = s.pickVariable();
-        System.out.println("Assign " + v.toString());
-        assigned.add(v);
-        if (dpllSatisfiable(s.assign(v), assigned) && numTrueMatch(assigned)) {
+        if (dpllSatisfiable(s.assign(v))) {
             return true;
         } else {
             v = new Variable(v.getSymbol(), !v.isNegated());
-            System.out.println("Assign " + v.toString());
-            assigned.add(v);
-            return dpllSatisfiable(s.assign(v), assigned);
+            return dpllSatisfiable(s.assign(v));
         }
-    }
-
-    private static boolean numTrueMatch(List<Variable> assigned) {
-        int count = 0;
-        for (Variable v : assigned) {
-            if (!v.isNegated()) {
-                count++;
-            }
-        }
-        return count == numTrue;
     }
 
     public static void main(String[] args) {
@@ -66,8 +41,7 @@ public class DPLL {
         CNFSentence s = new CNFSentence(repr);
         System.out.println("\nSentence: " + s.toString() + "\n");
         System.out.println("Steps:");
-        List<Variable> assigned = new ArrayList<>();
-        if (dpllSatisfiable(s, assigned)) {
+        if (dpllSatisfiable(s)) {
             System.out.println("\nSentence is satisfiable");
         } else {
             System.out.println("\nSentence is not satisfiable");
